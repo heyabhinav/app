4e41fd0e8e7d219bece3a3e9ee9671de22f9b794
heyabhinav <abhinav0050.be21@chitkara.edu.in>
2022-09-13 21:05:33 +0530

    Modified files, added products.

---
+++ pages/abhinav.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export default function abhinav(){
+    return(
+        <>
+            <div className='text-center font-bold text-4xl container mx-auto px-4'>
+                <h1>Hey! Abhinav here.</h1>
+            </div>
+        </>
+    )
+}
